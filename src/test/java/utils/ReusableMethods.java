5afa80ca354e34aba3ecab53a864c5f97b0cddd4
pc <muhammedmermer2004@gmail.com>
2023-11-28 14:21:17 +0300

    HooksWeb

--- src/test/java/utils/ReusableMethods.java
+++ src/test/java/utils/ReusableMethods.java
@@ -1,22 +1,34 @@
 package utils;
 
 import io.appium.java_client.TouchAction;
+import io.appium.java_client.android.AndroidTouchAction;
 import io.appium.java_client.touch.offset.PointOption;
 
+import static io.appium.java_client.touch.offset.PointOption.point;
 
-public class ReusableMethods {
-    Driver driver = new Driver();
-    TouchAction action = new TouchAction(driver.getAndroidDriver()) ;
-    public void waitFor(int sec){
-        try{
+
+public class ReusableMethods extends Hooks {
+
+    AndroidTouchAction action;
+
+    public void waitFor(int sec) {
+        try {
             Thread.sleep(sec * 1000);
-        }catch (InterruptedException e){
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
 
+    public void tabOnThePoints(int startX, int startY) {
+        action = new AndroidTouchAction(androidDriver);
+        action.tap(point(startX, startY)).perform();
+    }
 
-    public void tabOnThePoints(int StartPoinx, int StartPointy){
-        action.tap(PointOption.point(StartPoinx,StartPointy)).perform();
+
+    public void scroll(int fromX, int fromY, int toX, int toY) {
+        action = new AndroidTouchAction(androidDriver);
+        action.longPress(point(fromX, fromY))
+                .moveTo(point(toX, toY)).release()
+                .perform();
     }
 }
